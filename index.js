cfcad8a3f1b4ff093301fb68d291e71d374f8a49
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-18 01:20:18 +0200

    Connection to MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const connectDB = require('./config/database');
 
 dotenv.config();
 
 const port = process.env.PORT || 4001;
+
+// Connectez-vous à MongoDB
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello every developer');
